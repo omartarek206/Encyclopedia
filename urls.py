6bf98738bf5d2d1ceb129bc015ef7bc2272cf375
omartarek206 <40853054+omartarek206@users.noreply.github.com>
2020-10-02 15:42:08 +0200

    V0.44 Search is fully functional. Redirection when exact entry is entered not yet implemented ... Add Fully implemented.

--- urls.py
+++ urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("search", views.search, name="search"),
+    path("new", views.new, name="new"),
+
 
     path("<str:my_Entry>", views.visit_Entry, name="visit_Entry")
 ]
